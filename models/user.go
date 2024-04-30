0adb4945436a8cb6acbc4d8ddca24a90fe1a6056
svidzger <german.svidzinskij@gmail.com>
2024-04-30 20:16:30 +0300

    Refactor user struct to be more simple. Closes #6

--- models/user.go
+++ models/user.go
@@ -1,20 +1,16 @@
 package models
 
 import (
-	"database/sql"
 	"time"
 )
 
 type User struct {
-	ID        int            `json:"id"`
-	Username  string         `json:"username"`
-	Email     string         `json:"email"`
-	About     sql.NullString `json:"about"`
-	Password  string         `json:"password"`
-	AvatarURL sql.NullString `json:"avatar_url"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	Role      string         `json:"role"`
-	IsActive  bool           `json:"is_active"`
-	LastLogin sql.NullTime   `json:"last_login"`
+	ID        int       `json:"id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Role      string    `json:"role"`
+	IsActive  bool      `json:"is_active"`
 }
